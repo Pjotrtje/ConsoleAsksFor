7710ceaf9ff95f704618d43cfd138a666d6bb72c
Pieter van Sambeek <pietervansambeek@hotmail.com>
2022-05-09 20:44:48 +0200

    Added AskForStringBasedValueObject

--- examples/ExampleApp/Apps/WipApp.cs
+++ examples/ExampleApp/Apps/WipApp.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 using ConsoleAsksFor.NodaTime.ISO;
 
@@ -15,6 +16,21 @@ internal sealed class WipApp : IApp
         _console = console;
     }
 
+    private sealed record Agb(string Value)
+    {
+        public static bool TryParse(string value, [MaybeNullWhen(false)] out Agb agb)
+        {
+            if (value.All(char.IsNumber) && value.Length == 8)
+            {
+                agb = new Agb(value);
+                return true;
+            }
+
+            agb = null;
+            return false;
+        }
+    }
+
     public async Task Run()
     {
         //var nowTimeOnly = TimeOnly.FromDateTime(DateTime.Now);
@@ -25,7 +41,8 @@ internal sealed class WipApp : IApp
 
         _console.WriteSplitter(ConsoleColor.Blue);
         _console.WriteCustomLine("sdf", ConsoleColor.DarkCyan);
-        var xx = await _console.AskForItems("", new List<string> { "", "A", "B" });
+        await _console.AskForItems("", new List<string> { "", "A", "B" });
+        await _console.AskForStringBasedValueObject<Agb>("Agb", Agb.TryParse, x => x.ToString(), "8 numbers");
         await _console.AskForLocalTime("How late?", Between(new LocalTime(11, 01, 00, 01), new LocalTime(11, 02, 00, 02)));
 
         await _console.AskForTimeOnly("When?", Between(new TimeOnly(11, 01, 00, 01), new TimeOnly(11, 01, 00, 02)));
---
+++ src/ConsoleAsksFor/Questions/String/AskForStringBasedValueObject.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleAsksFor;
+
+public static partial class AskForAppender
+{
+    /// <summary>
+    /// Ask for string based value object.
+    /// </summary>
+    /// <param name="console"></param>
+    /// <param name="questionText"></param>
+    /// <param name="tryParse"></param>
+    /// <param name="hint"></param>
+    /// <param name="defaultValue"></param>
+    /// <param name="cancellationToken"></param>
+    /// <param name="toString"></param>
+    /// <returns></returns>
+    public static async Task<TValueType> AskForStringBasedValueObject<TValueType>(
+        this IConsole console,
+        string questionText,
+        TryParse<TValueType> tryParse,
+        Func<TValueType, string> toString,
+        string? hint,
+        string? defaultValue = null,
+        CancellationToken cancellationToken = default)
+        where TValueType : notnull
+    {
+        var question = new StringBasedValueObject<TValueType>(
+            questionText,
+            tryParse,
+            toString,
+            hint,
+            defaultValue);
+
+        return await console.Ask(question, cancellationToken);
+    }
+}
