f46e3fd32d3c8ee38683b8d0d6a21dee93dc0bea
Pieter van Sambeek <pietervansambeek@hotmail.com>
2022-12-30 19:16:22 +0100

    Crap

---
+++ tests/ConsoleAsksFor.NodaTime.ISO.Tests/IntellisenseUseCase.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace ConsoleAsksFor.TestUtils;
+
+public sealed record IntellisenseUseCases
+{
+    public sealed record FromUserInput
+    {
+        public string Input { get; init; } = null!;
+        public string? Previous { get; init; }
+        public string? Complete { get; init; }
+        public string? Next { get; init; }
+        public string UseCase { get; init; } = null!;
+
+        public string FullUseCaseCase => $"{UseCase} (UserInput)";
+    }
+
+    public sealed record FromIntellisenseInput
+    {
+        public string Input { get; init; } = null!;
+        public string Hint { get; init; } = null!;
+        public string? Previous { get; init; }
+        public string? Next { get; init; }
+        public string UseCase { get; init; } = null!;
+
+        public string FullUseCaseCase => $"{UseCase} (Intellisense)";
+    }
+
+    public IReadOnlyCollection<FromUserInput> FromUser { get; init; } = Array.Empty<FromUserInput>();
+    public IReadOnlyCollection<FromIntellisenseInput> FromIntellisense { get; init; } = Array.Empty<FromIntellisenseInput>();
+
+    public TheoryData<string, string, string?> CompleteValueUseCases
+    {
+        get
+        {
+            var data = new TheoryData<string, string, string?>();
+            foreach (var item in FromUser)
+            {
+                data.Add(item.FullUseCaseCase, item.Input, item.Complete ?? item.Next);
+            }
+            return data;
+        }
+    }
+
+    public TheoryData<string, string, string, string?> PreviousValueUseCases
+    {
+        get
+        {
+            var data = new TheoryData<string, string, string, string?>();
+            foreach (var item in FromIntellisense)
+            {
+                data.Add(item.FullUseCaseCase, item.Input, item.Hint, item.Previous);
+            }
+
+            foreach (var item in FromUser)
+            {
+                data.Add(item.FullUseCaseCase, item.Input, item.Input, item.Previous);
+            }
+            return data;
+        }
+    }
+
+    public TheoryData<string, string, string, string?> NextValueUseCases
+    {
+        get
+        {
+            var data = new TheoryData<string, string, string, string?>();
+            foreach (var item in FromIntellisense)
+            {
+                data.Add(item.FullUseCaseCase, item.Input, item.Hint, item.Next);
+            }
+
+            foreach (var item in FromUser)
+            {
+                data.Add(item.FullUseCaseCase, item.Input, item.Input, item.Next);
+            }
+            return data;
+        }
+    }
+}
